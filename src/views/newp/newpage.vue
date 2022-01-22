87c7d944358a3901caff9a5b445dfb4f06e73ff7
wangwh <wangwh@qaics.com>
2022-01-22 17:10:47 +0800

    初始化

---
+++ src/views/newp/newpage.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+
+ 
+  </div>
+</template>
+<script>
+
+export default {
+  name: "newpage",
+  data() {
+    return {
+      currentDate: new Date(),
+    };
+  },
+  created() {},
+
+  methods: {
+  
+  
+  }
+};
+</script>
+<style lang="less" scoped>
+</style>
